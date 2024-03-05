3434e499e67d658ea40b10fff77c1524a4bd21a8
juiyu cheng <r12723030@ntu.edu.tw>
2024-03-05 21:09:12 +0800

    amend  date d1(29, 2, 2023); change name

--- ConsoleApplication1/main.cpp
+++ ConsoleApplication1/main.cpp
@@ -3,8 +3,8 @@
 #include <iostream>
 
 int main() {
-    date d1(29, 2, 2021); // Create a date: December 25, 2021
-    date d2(29, 2, 2024);   // Create another date: January 1, 2022
+    date d1(29, 2, 2023);
+    date d2(29, 2, 2024); 
 
     // Check if dates are valid
     std::cout << "Date d1 is " << (d1.valid() ? "valid" : "invalid") << std::endl;
@@ -31,14 +31,3 @@ int main() {
     return 0;
 }
 
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
